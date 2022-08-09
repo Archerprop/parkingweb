d9bd1fce563f4dc1a48e3285f93f7e2dfc83e0eb
Archerprop <archerprop@outlook.com>
2022-08-08 21:41:26 -0500

    estilos

--- website/console.php
+++ website/console.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/ico" href="../sources/images/carLogo.ico">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
     <script src="../js/sweetalert2.all.min.js"></script>
 
     <title>ParkingWeb</title>
